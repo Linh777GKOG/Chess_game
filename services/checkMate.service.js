7a43f959421566a4fe8370e7568efc215bd56cd0
Linh Vu <linh777gkog@gmail.com>
2022-06-08 22:36:29 +0700

    check point

--- services/checkMate.service.js
+++ services/checkMate.service.js
@@ -3,4 +3,25 @@ import { $, $$$, deepclone } from "../utils/utils.js";
 import { chessConfig } from "../config/chessConfig.config.js";
 import { playerTurn } from "./playerTurn.sevices.js";
 
-export const checkMate
+export const checkMate = {
+  gameOver: false,
+
+  cantMoveDueToCheck({ pieceSelectedPosition, pieceBoxPosition }) {
+    const pieceBoxElementSelected = $(`#${pieceSelectedPosition}`);
+    const pieceElementSelected = $$$(
+      pieceBoxElementSelected,
+      chessConfig.chessPieceSelector
+    );
+
+    const clonedPieceElement = pieceElementSelected.cloneNode();
+    const tempPieceBoxElement = $(`#${pieceBoxPosition}`);
+    const tempPieceElement = $$$(
+      tempPieceBoxElement,
+      chessConfig.chessPieceBoxSelector
+    );
+    if (tempPieceElement) document.body.append(tempPieceElement);
+
+    tempPieceBoxElement.append(clonePieceElement);
+    pieceElementSelected.remove();
+  },
+};
