fdd2e31f20efabee8bd48d025ae856c388bb8db3
Linh Vu <linh777gkog@gmail.com>
2022-06-14 23:07:55 +0700

    commit

---
+++ services/piecesHandle/handlePieceStates.methods.js
@@ -0,0 +1,9 @@
+const classesSelectors = {
+  pieceSelected: 'piece-selected',
+  pieceNotAllowed: 'piece-not-allowed',
+  pieceReady: 'piece-ready',
+  piecePotential: 'piece-potential',
+  piecePointer: 'piece-pointer',
+};
+
+const methodsSelectors = [{ set: 'add' }, { remove: 'remove' }];
