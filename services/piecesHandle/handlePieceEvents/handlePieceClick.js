c4c00ce01d009d0e3097ae65cba084efb15aa063
Linh Vu <linh777gkog@gmail.com>
2022-06-14 23:14:01 +0700

    commit

--- services/piecesHandle/handlePieceEvents/handlePieceClick.js
+++ services/piecesHandle/handlePieceEvents/handlePieceClick.js
@@ -4,63 +4,79 @@ import { piecesDetermine } from '../../piecesDetermine.service.js';
 import handlePieceMovingHelpers from '../helpers/handlePieceMoving.helpers.js';
 
 export default {
-    handlePieceClick({ pieceBoxElement, pieceBoxPosition, pieceElement, pieceType }) {
-        if ( checkMate.gameOver ) {
-            return
-        }
+  handlePieceClick({
+    pieceBoxElement,
+    pieceBoxPosition,
+    pieceElement,
+    pieceType,
+  }) {
+    if (checkMate.gameOver) {
+      return;
+    }
 
-        const { pieceSelectedPosition } = this
-        
-        if ( piecesDetermine.hasPiecePotential( pieceSelectedPosition, pieceBoxPosition ) ) {
-            if ( checkMate.cantMoveDueToCheck({ pieceSelectedPosition, pieceBoxPosition }) ) {
-                this.setNotAllowed( pieceBoxElement )
-                return
-            }
-            
-            this.handleMovingThePiece({ pieceBoxElement, pieceElement })
+    const { pieceSelectedPosition } = this;
 
-            if ( checkMate.isCheckMate() ) {
-                checkMate.displayCheckMateMessage()
-            }
-            return
-        }
+    if (
+      piecesDetermine.hasPiecePotential(pieceSelectedPosition, pieceBoxPosition)
+    ) {
+      if (
+        checkMate.cantMoveDueToCheck({
+          pieceSelectedPosition,
+          pieceBoxPosition,
+        })
+      ) {
+        this.setNotAllowed(pieceBoxElement);
+        return;
+      }
 
-        if ( !pieceElement ) {
-            return
-        }
-        if ( 
-            this.isPieceSelected() && 
-            this.isNotOnPieceSelected( pieceBoxPosition )
-        ) {
-            return
-        }
+      this.handleMovingThePiece({ pieceBoxElement, pieceElement });
 
-        if ( 
-            this.isOnPieceSelected( pieceBoxPosition )
-        ) {
-            this.resetPieceSelected()
-            this.removeReady( pieceBoxElement )
-            this.removePiecePotentials( pieceBoxPosition )
-            
-            return
-        } 
+      if (checkMate.isCheckMate()) {
+        checkMate.displayCheckMateMessage();
+      }
+      return;
+    }
 
-        if ( 
-            playerTurn.isRightTurn( pieceType )
-        ) {
-        
-            if ( this.hasPiecePotentials( pieceBoxPosition )) {
-                this.changePieceSelected( pieceBoxPosition )
-                this.setReady( pieceBoxElement )
-                this.setPiecePotentials( pieceBoxPosition )
-            } else {
-                this.setNotAllowed( pieceBoxElement ) 
-            }
-            
-            return
-        }
-        else if ( !this.isPieceSelected() ) {
-            this.setNotAllowed( pieceBoxElement )
-            return
-        }
-    },
+    if (!pieceElement) {
+      return;
+    }
+    if (this.isPieceSelected() && this.isNotOnPieceSelected(pieceBoxPosition)) {
+      return;
+    }
+
+    if (this.isOnPieceSelected(pieceBoxPosition)) {
+      this.resetPieceSelected();
+      this.removeReady(pieceBoxElement);
+      this.removePiecePotentials(pieceBoxPosition);
+
+      return;
+    }
+
+    if (playerTurn.isRightTurn(pieceType)) {
+      if (this.hasPiecePotentials(pieceBoxPosition)) {
+        this.changePieceSelected(pieceBoxPosition);
+        this.setReady(pieceBoxElement);
+        this.setPiecePotentials(pieceBoxPosition);
+      } else {
+        this.setNotAllowed(pieceBoxElement);
+      }
+
+      return;
+    } else if (!this.isPieceSelected()) {
+      this.setNotAllowed(pieceBoxElement);
+      return;
+    }
+  },
+
+  setPiecePotentials(pieceBoxPosition) {
+    piecesDetermine.itereateDetermine(pieceBoxPosition, this.setPotential);
+  },
+  removePiecePotentials(pieceBoxPosition) {
+    piecesDetermine.itereateDetermine(pieceBoxPosition, this.removePotential);
+  },
+  hasPiecePotentials(pieceBoxPosition) {
+    return piecesDetermine.hasPiecePotentials(pieceBoxPosition);
+  },
+
+  ...handlePieceMovingHelpers,
+};
