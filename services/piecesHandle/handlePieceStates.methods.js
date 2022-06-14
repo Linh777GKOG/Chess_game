1470e01c429f803bc89b98ad982b16c3fc704f67
Linh Vu <linh777gkog@gmail.com>
2022-06-14 23:08:15 +0700

    commit

--- services/piecesHandle/handlePieceStates.methods.js
+++ services/piecesHandle/handlePieceStates.methods.js
@@ -7,3 +7,24 @@ const classesSelectors = {
 };
 
 const methodsSelectors = [{ set: 'add' }, { remove: 'remove' }];
+
+export default ((_) => {
+  const handlePieceStatesMethods = {};
+
+  for (const classkey in classesSelectors) {
+    const methodName = classkey.replace('piece', '');
+
+    methodsSelectors.map((value) => {
+      const methodSelector = `${Object.keys(value)[0]}${methodName}`;
+      const classMethodSelector = `${Object.values(value)[0]}`;
+
+      handlePieceStatesMethods[methodSelector] = (pieceBoxElement) => {
+        pieceBoxElement.classList[classMethodSelector](
+          classesSelectors[classkey]
+        );
+      };
+    });
+  }
+
+  return handlePieceStatesMethods;
+})();
