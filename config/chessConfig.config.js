073773f72dcb47267e811e1b6d6ca4da3fe56647
Linh Vu <linh777gkog@gmail.com>
2022-06-14 23:00:56 +0700

    commit

--- config/chessConfig.config.js
+++ config/chessConfig.config.js
@@ -1,10 +1,10 @@
 export const chessConfig = {
-  chessTableSelector: ".chess-table",
-  chessPieceBoxSelector: ".piece-box",
-  chessPieceSelector: ".piece",
-  blackWinsSelector: ".chess-message .black-wins",
-  whiteWinsSelector: ".chess-message .white-wins",
-  whitePlayDown: false,
+  chessTableSelector: '.chess-table',
+  chessPieceBoxSelector: '.piece-box',
+  chessPieceSelector: '.piece',
+  blackWinsSelector: '.chess-message .black-wins',
+  whiteWinsSelector: '.chess-message .white-wins',
+  whitePlaysDown: false,
   useInitialGame: false,
 };
 
